cf948e3b00cfc8ad5fb60ae42ebbe93b5aa076f5
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2024-11-18 11:10:43 +0530

    Base setup of ios app without storyboards

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Dwarakanatha Reddy Poreddy on 18/11/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
