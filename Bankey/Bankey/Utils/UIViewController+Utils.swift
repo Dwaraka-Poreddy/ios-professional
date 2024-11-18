67403f46b6de365c114dead339b147a559ef33f8
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2024-11-18 17:31:26 +0530

    Adds Main page tab bar base setup

---
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Dwarakanatha Reddy Poreddy on 18/11/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance =  navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
