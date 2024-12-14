1b8d766627c50ff98f8d5f9ee4b41f12d24b8eab
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2024-12-14 22:35:22 +0530

    feat: Add networking to Account Summary

---
+++ Bankey/Bankey/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  Bankey
+//
+//  Created by Dwarakanatha Reddy Poreddy on 14/12/24.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
